455104834340973835e2c9153a68901a750e8ec6
Pauli Sairanen <pauli.sairanen@spatineo.com>
2021-09-24 18:13:34 +0300

    uncommented openDevTools

--- electron/index.ts
+++ electron/index.ts
@@ -22,7 +22,7 @@ const createWindow = () => {
     window.loadFile(`file://${__dirname}/index.html`)
   }
   window.loadFile(`${__dirname}/index.html`)
-  // window.webContents.openDevTools()
+  window.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
